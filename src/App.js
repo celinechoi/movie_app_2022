cd393358270359ac27a9769b856bf94b6ea003a2
celinechoi <web.jinseul@gmail.com>
2022-02-17 17:28:33 +0900

    key props 에러 디버깅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Food({ name, picture }) {
+function Food({name, picture }) {
 	return (
 		<div>
 			<h2>I like { name }</h2>
@@ -11,22 +11,27 @@ function Food({ name, picture }) {
 
 const foodILike = [
 	{
+		id: 1,
 		name: 'Kimchi',
 		image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IDrNtl4pG_zepTDko3IUhgHaHa%26pid%3DApi%26h%3D160&f=1',
 	},
 	{
+		id: 2,
 		name: 'Samgyeopsal',
 		image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.z1NRYI5oQWdaIQQ7QJwhjAHaHa%26pid%3DApi%26h%3D160&f=1'
 	},
 	{
+		id: 3,
 		name: 'Bibimbap',
 		image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.PGMADTeqQpHS4FZiUJ4T1wHaE8%26pid%3DApi%26h%3D160&f=1'
 	},
 	{
+		id: 4,
 		name: 'Doncasu',
 		image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UpDCozCT4DXb6y8AvIzGgAHaE6%26pid%3DApi%26h%3D160&f=1'
 	},
 	{
+		id: 5,
 		name: 'Kimbap',
 		image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.eb8dner0MAzd2wx3n69F9wHaEK%26pid%3DApi%26h%3D160&f=1'
 	},
@@ -35,8 +40,8 @@ const foodILike = [
 function App() {
   return (
 		<div>
-			{foodILike.map(dish => (
-				<Food name={dish.name} picture={dish.image}/>
+			{foodILike.map((dish, index) => (
+				<Food name={dish.name} picture={dish.image} key={index} />
 			))}
 		</div>
 	);
