0fc13a98555b0a71b25c7153bc7787c08548fcea
celinechoi <web.jinseul@gmail.com>
2022-02-18 17:22:05 +0900

    06-2 영화 데이터 화면에 그리기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+	// 데이터 저장
 	state= {
 		isLoading: true,
 		movies: [],
 	}
+	getMoives = async () => {
+		const {
+			data: {
+				data: {movies}, // satate 키와 axios.get() 대입할 변수가 같을 때 축약 {movies: movies} 
+			}
+		} = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+		this.setState({movies, isLoading: false});
+	}
 	componentDidMount() {
-		// 영화 데이터 로딩!
-		setTimeout(() => {
-			this.setState({isLoading: false});
-		}, 6000);
+		this.getMoives();
 	}
 	render() {
 		const {isLoading} = this.state;
