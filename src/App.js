f8e2b4ad912c509251245eae5e313cd52f713212
celinechoi <web.jinseul@gmail.com>
2022-02-22 09:55:07 +0900

    07 영화 앱 다듬기

--- src/App.js
+++ src/App.js
@@ -28,20 +28,20 @@ class App extends React.Component {
 	render(){
 		const {isLoading, movies} = this.state;
 		return (
-			<section class="container">
+			<section className="container">
 				{
 					isLoading? 
 					(
-						<div class="loader">
-							<span class="loader__text">Loading...</span>
+						<div className="loader">
+							<span className="loader__text">Loading...</span>
 						</div>
 						)
 					:
 					( 
-						<div class="movies">
+						<div className="movies">
 						{
 							movies.map((movie) => (
-								<Movie key={movie.id} title={movie.title} year={movie.year} summary={movie.summary} poster={movie.medium_cover_image} />
+								<Movie key={movie.id} title={movie.title} year={movie.year} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
 							))
 						}
 						</div>
--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Proptypes from 'prop-types';
 import './Movie.css'
 // 함수형 컴포넌트
-function Movie({title, year, summary, poster}) {
+function Movie({title, year, summary, poster, genres}) {
 	return (
-		<div class="movie">
+		<div className="movie">
 			<img src={poster} alt={title} />
-			<div class="movie__data">
-				<h3 class="movie__title" style={{backgroundColor: 'red'}}>{title}</h3>
-				<h5 class="movie__year">{year}</h5>
-				<p class="movie__summary">{summary}</p>		
+			<div className="movie__data">
+				<h3 className="movie__title" style={{backgroundColor: 'red'}}>{title}</h3>
+				<h5 className="movie__year">{year}</h5>
+				<ul className="movie__genres">
+					{
+						genres.map((genre, i) => {
+							return <li key={i}>{genre}</li>
+						})
+					}
+				</ul>
+				<p className="movie__summary">{summary.slice(0, 180)}...</p>
 			</div>
 		</div>
 	)
@@ -18,6 +25,7 @@ Movie.prototype = {
 	title: Proptypes.string.isRequired,
 	year: Proptypes.number.isRequired,
 	summary: Proptypes.string.isRequired,
-	poster: Proptypes.string.isRequired
+	poster: Proptypes.string.isRequired,
+	genres: Proptypes.arrayOf(Proptypes.string).isRequired
 }
 export default Movie;
